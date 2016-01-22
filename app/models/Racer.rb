bed32b52e1f34acbfd11f32a9e4613747953c28a
montekaka <josh.chen.jia@gmail.com>
2016-01-21 22:27:11 -0800

    add a all class method

--- app/models/Racer.rb
+++ app/models/Racer.rb
@@ -8,4 +8,14 @@ class Racer
 	def self.collection
 		self.mongo_client['racers']
 	end	
+
+	def self.all(prototype={}, sort={}, skip=0, limit=nil)		
+		# temp = {} #hash needs to stay in stable order provided
+		# sort.each do {|k,v|			
+		# 	tmp[k] = v if [:first_name, :last_name, :gender, :group, :secs].include?(k)
+		# }
+		# sort=tmp
+		# prototype.each_with_object({}) {|(k,v), tmp| tmp[k.to_sym] = v; tmp}
+		collection.find.projection({_id:true, first_name:true, last_name:true, gender:true, group:true, secs:true})
+	end
 end
