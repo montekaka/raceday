81dd6c9c90a19f5d8dbd15d179b87f2c0eb597db
montekaka <josh.chen.jia@gmail.com>
2016-01-21 22:55:23 -0800

    add sort to all class method

--- app/models/Racer.rb
+++ app/models/Racer.rb
@@ -10,12 +10,20 @@ class Racer
 	end	
 
 	def self.all(prototype={}, sort={}, skip=0, limit=nil)		
-		# temp = {} #hash needs to stay in stable order provided
-		# sort.each do {|k,v|			
-		# 	tmp[k] = v if [:first_name, :last_name, :gender, :group, :secs].include?(k)
-		# }
-		# sort=tmp
-		# prototype.each_with_object({}) {|(k,v), tmp| tmp[k.to_sym] = v; tmp}
-		collection.find.projection({_id:true, first_name:true, last_name:true, gender:true, group:true, secs:true})
+		tmp = {} #hash needs to stay in stable order provided
+		sort.each {|k,v| 		
+			tmp[k] = v  if [:first_name, :last_name, :gender, :group, :secs].include?(k)
+		}
+		sort=tmp
+		Rails.logger.debug {"getting all zips, prototype=#{prototype}, sort=#{sort}, skip=#{skip}, limit=#{limit}"}
+
+		prototype.each_with_object({}) {|(k,v), tmp| tmp[k.to_sym] = v; tmp}
+		result=collection.find(prototype)
+			.projection({_id:true, first_name:true, last_name:true, gender:true, group:true, secs:true})
+			.sort(sort)
+			.skip(skip)
+		result = result.limit(limit) if !limit.nil?
+		return result
+		#collection.find.projection({_id:true, first_name:true, last_name:true, gender:true, group:true, secs:true})
 	end
 end
